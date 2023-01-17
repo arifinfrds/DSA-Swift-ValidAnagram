0ea886902075bc31838cd341115ab214733067c8
arifinfrds <arifinfrds.engineer@gmail.com>
2023-01-17 08:17:50 +0700

    `isAnagram` returns true on anagram

--- ValidAnagramTests/ValidAnagramTests.swift
+++ ValidAnagramTests/ValidAnagramTests.swift
@@ -10,7 +10,36 @@ import XCTest
 
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-        return false
+        
+        if s.isEmpty && t.isEmpty {
+            return false
+        }
+        
+        var firstHashTable = [Character: Int]()
+        
+        for character in s {
+            if let count = firstHashTable[character], count == 1 {
+                firstHashTable[character] = count + 1
+            } else {
+                firstHashTable[character] = 1
+            }
+        }
+        
+        var secondHashTable = [Character: Int]()
+        
+        for character in t {
+            if let count = secondHashTable[character], count == 1 {
+                secondHashTable[character] = count + 1
+            } else {
+                secondHashTable[character] = 1
+            }
+        }
+        
+        if firstHashTable == secondHashTable {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
@@ -54,5 +83,13 @@ final class ValidAnagramTests: XCTestCase {
         let result = sut.isAnagram("rat", "car")
         
         XCTAssertEqual(result, false)
+    }
+    
+    func test_isAnagram_returnsTrueOnAnagram() {
+        let sut = Solution()
+        
+        let result = sut.isAnagram("anagram", "nagaram")
+        
+        XCTAssertEqual(result, true)
     }
 }
